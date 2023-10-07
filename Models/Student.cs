c0eb65a6e2bab3d90b8c1aada33a4c4ad9a8ed46
Gandrakota Dhiraj venkata phani saikrishna <82799252+dhirajsai135@users.noreply.github.com>
2023-10-07 22:38:09 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManagementAPI.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int Age { get; set; }
+    public required string Class { get; set; }
+    public required string Address { get; set; }
+}
---
+++ Models/StudentContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagementAPI.Models;
+
+public class StudentContext : DbContext
+{
+    public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Student> Students { get; set; }
+}
